f4533e9f5d626ce70ff23121623b016ad1ed298e
ZuoTso <chiushengzuo@gmail.com>
2023-10-01 22:16:42 +0800

    Add myStackTest

---
+++ myStackTest.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void StackPush(char* stack,char nextElement);
+char* StackPop(char* stack);
+char StackTop(char* stack);
+int StackIsEmpty(char* stack);
+void StackFull(char* stack);
+
+int main(int argc, char *argv[]){
+	
+}
